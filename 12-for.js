706497a71b30a3537089f19a27932f2599a93640
Tiago Rodrigues <tiagorodriguesnutri@gmail.com>
2025-01-03 21:05:33 -0300

    Removendo arquivos antigos

--- 12-for.js
+++
@@ -1,9 +0,0 @@
-const listaDeNumeros = [33, 22, 555, 25, -10, 0]
-
-for (let contador = 0; contador < 100; contador += 1) {
-	console.log(contador)
-}
-for (let contador = 0; contador < listaDeNumeros.length; contador += 1) {
-	console.log(listaDeNumeros[contador])
-}
-
